83ecdcd7f0c0207562546e0c60893579f171bd85
BuildTools <unconfigured@null.spigotmc.org>
2018-08-27 23:59:54 -0700

    Got interupted working on BSTtree insert

--- BSTtree/BSTtree/Program.cs
+++ BSTtree/BSTtree/Program.cs
@@ -27,8 +27,43 @@ namespace BSTtree
         }
     }
     
-    class BSTtreeInterface
+    class BSTtreeInterface //test for the interface class. add more to this.
     {
 
+    }
+
+    class BSTtree : BSTtreeInterface //actual implementation of the Binary Tree, put this in another file.w
+    {
+        private class BSTnode
+        {
+            public BSTnode left = null;
+            public BSTnode right = null;
+            public int data = -1; //picked -1 arbritarily as being no data as its outside of the user input range.
+            public int height = 0; //variable that stores the current height of the node
+
+
+            //initializes class members from contrsuctor arguments
+            BSTnode(int input, BSTnode inputLeft = null, BSTnode inputRight = null)
+            {
+                data = input;
+                left = inputLeft;
+                right = inputRight;
+            }
+
+            void setHeight(int inputHeight)
+            {
+                if (height < inputHeight)
+                    height = inputHeight;
+            }
+        }
+
+        private BSTnode root = null; //creates an empty root node to start the tree.
+
+        private int insert(int input, BSTnode node)
+            {
+            if (node == null)
+                node = new BSTnode(input); //trying to make a recursive function to insert into the tree.
+                return node.height;
+            }
     }
 }
