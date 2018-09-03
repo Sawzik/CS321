8ebb4270243d98851f5236e96d69f6eabd86a2d8
BuildTools <unconfigured@null.spigotmc.org>
2018-09-02 20:04:07 -0700

    Added all functions of a BSTtree. Not tested yet

--- BSTtree/BSTtree/BSTtreeClass.cs
+++ BSTtree/BSTtree/BSTtreeClass.cs
@@ -34,6 +34,30 @@ namespace BSTtree
             return node.height;
         }
 
+        private BSTnode RemoveRoot(ref BSTnode node)
+        {
+            BSTnode newNode = null;
+
+            if (node.left != null && node.right != null) //two children
+            {
+                newNode = node;
+
+                BSTnode min = FindMin(node.right);
+                node.data = min.data; //finds the minimum node on the right subtree and copies its data into this node
+                Remove(node.data, ref min); //deletes the node that was brought up
+
+                return newNode;
+            }
+            else if (node.left == null) //one right child
+                newNode = node.right;
+            else if (node.right == null) //one left child
+                newNode = node.left;
+
+            node = null; //no children
+
+            return newNode;
+        }
+
         private void Remove(int rmData, ref BSTnode node)
         {
             if (node == null) //data not found
@@ -48,22 +72,60 @@ namespace BSTtree
                     node = node.left;
                 else //two children
                 {
-                    ref BSTnode min = ref FindMin(ref node.right);
+                    BSTnode min = FindMin(node.right);
                     node.data = min.data; //finds the minimum node on the right subtree and copies its data into this node
                     Remove(node.data, ref min); //deletes the node that was brought up
                 }
             }
-            else if (node.data < rmData) 
+            else if (node.data < rmData)
                 Remove(rmData, ref node.left); //traverses left subtree
             else
                 Remove(rmData, ref node.right); //traverses right subtree
         }
 
-        private ref BSTnode FindMin(ref BSTnode node)
+        private BSTnode FindMin(BSTnode node)
         {
             if (node.left == null) //reached the leftmost node
-                return ref node;
-            return ref FindMin(ref node.left);
+                return node;
+            return FindMin(node.left);
+        }
+
+        private BSTnode FindMax(BSTnode node)
+        {
+            if (node.left == null) //reached the leftmost node
+                return node;
+            return FindMax(node.right);
+        }
+
+        private BSTnode Find(int input, BSTnode node)
+        {
+            if (node == null)
+                return null;
+
+            if (input == node.data)
+                return node;
+
+            if (input < node.data)
+                return Find(input, node.left);
+
+            return Find(input, node.right);
+        }
+
+        private bool Contains(int input, BSTnode node)
+        {
+            if (Find(input, node) == null)
+                return false;
+            return true;
+        }
+
+        void MakeEmpty(BSTnode node)
+        {
+            if (node != null)
+            {
+                MakeEmpty(node.left);
+                MakeEmpty(node.right);
+                node = null;
+            }
         }
 
         private void PrintTree(ref BSTnode node)
@@ -74,7 +136,37 @@ namespace BSTtree
                 Console.Write(node.data + " ");
                 PrintTree(ref node.right);
             }
-        }        
+        }
+
+        ~BSTtree()
+        {
+            MakeEmpty(root);
+        }
+
+        public int FindMin()
+        {
+          return FindMin(root).data;
+        }
+
+        public int FindMax()
+        {
+            return FindMax(root).data;
+        }
+
+        public int Find(int input)
+        {
+            return Find(input, root).data;
+        }
+
+        public bool Contains(int input)
+        {
+            return Contains(input, root);
+        }
+
+        public bool IsEmpty()
+        {
+            return root == null;
+        }
 
         public void PrintTree()
         {
@@ -84,6 +176,14 @@ namespace BSTtree
         public void Insert(int input)
         {
             Insert(input, ref root);
+        }
+
+        public void Remove(int input)
+        {
+            if (root.data == input)
+                root = RemoveRoot(ref root);
+            else
+                Remove(input, ref root);
         }
 
         public int Count()
