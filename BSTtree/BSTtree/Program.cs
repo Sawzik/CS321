d8b953e4398a4df9c0e57903efa4088222950348
BuildTools <unconfigured@null.spigotmc.org>
2018-09-02 17:10:13 -0700

    Working insert and basic public functions

--- BSTtree/BSTtree/Program.cs
+++ BSTtree/BSTtree/Program.cs
@@ -8,22 +8,25 @@ namespace BSTtree
 {
     class Program
     {
-        static void Main(string[] args) //Main of the program
+        static void Main(string[] args)
         {
+            BSTtree tree = new BSTtree();
             Console.WriteLine("test input by typing numbers. no error detection");
 
             string input = Console.ReadLine(); //reading input from user. Gets the whole line
-
             string[] parsedInput = input.Split(' ');
 
             Console.WriteLine(input);
 
-            foreach (string s in parsedInput) //use this loop to add each element into the tree.
-            {
-                Console.WriteLine(s); //writes to the console one line at a time.
+            foreach (string s in parsedInput) 
+            {  
+                Console.WriteLine(s);
+                tree.Insert(Int32.Parse(s));
             }
 
-            Console.ReadKey(); //pausing at the end of the program
+            tree.PrintTree();
+
+            Console.ReadKey(); 
         }
     }
 }
