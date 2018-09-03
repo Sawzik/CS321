0b5a047db40e6d253083c1e789331d2e314cb481
BuildTools <unconfigured@null.spigotmc.org>
2018-09-02 18:49:47 -0700

    Git said .cs files werent in the commit. Added them.

---
+++ BSTtree/BSTtree/BSTnodeClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSTtree
+{
+    class BSTnode : ICloneable, IComparable<BSTnode>
+    {
+        public BSTnode left = null;
+        public BSTnode right = null;
+        public int data = -1; //picked -1 arbritarily as being no data as its outside of the user input range.
+        public int height = 0; //variable that stores the current height of the node
+
+        public BSTnode(int input)
+        {
+            data = input;
+            left = null;
+            right = null;
+        }
+
+        public BSTnode(int input, BSTnode inputLeft, BSTnode inputRight)
+        {
+            data = input;
+            left = inputLeft;
+            right = inputRight;
+        }
+
+        public object Clone()
+        {
+            BSTnode clonedLeft = null;
+            BSTnode clonedRight = null;
+            if (this.left != null) //Recusively calling clone on each child for a deep copy.
+                clonedLeft = this.left.Clone() as BSTnode; //clone takes an object class, telling it that its taking a BSTnode which is inherits from object 
+            if (this.right != null)
+                clonedRight = this.right.Clone() as BSTnode;
+            return new BSTnode(this.data, clonedLeft, clonedRight);
+        }
+
+        public int CompareTo(BSTnode other)
+        {
+            if (this.data < other.data)
+                return -1;
+            else if (this.data == other.data)
+                return 0;
+            //if (this.data > other.data)
+            return 1;
+        }
+
+        public void SetHeight(int inputHeight)
+        {
+            if (height < inputHeight)
+                height = inputHeight;
+        }
+    }
+}
---
+++ BSTtree/BSTtree/BSTtreeClass.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSTtree
+{
+    class BSTtree
+    {
+
+        private BSTnode root = null; //creates an empty root node to start the tree.
+        private int nodes = 0;
+        private int depth = 0;
+
+        //recursively calls itself until it finds where to add a new node. increases the height each time
+        private int Insert(int input, ref BSTnode node)
+        {
+            if (node == null)
+            {
+                nodes++; //keeps track of the number of nodes
+                node = new BSTnode(input);
+            }
+            else if (input < node.data)
+                node.SetHeight(Insert(input, ref node.left) + 1);
+            else if (input == node.data) //if data is already in the tree
+                return -1;
+            else
+                node.SetHeight(Insert(input, ref node.right) + 1);
+
+            if (node.height > depth) //keeps track of the deepest node
+                depth = node.height;
+
+            return node.height;
+        }
+
+        private void Remove(int rmData, ref BSTnode node)
+        {
+            if (node == null) //data not found
+                return;
+            if (rmData == node.data) //data found
+            {
+                if (node.left == null && node.right == null) //no children
+                    node = null; //relying on the garbage collector to delete memory once there are no more references.
+                else if (node.left == null) //one right child
+                    node = node.right;
+                else if (node.right == null) //one left child
+                    node = node.left;
+                else //two children
+                {
+                    ref BSTnode min = ref FindMin(ref node.right);
+                    node.data = min.data; //finds the minimum node on the right subtree and copies its data into this node
+                    Remove(node.data, ref min); //deletes the node that was brought up
+                }
+            }
+            else if (node.data < rmData) 
+                Remove(rmData, ref node.left); //traverses left subtree
+            else
+                Remove(rmData, ref node.right); //traverses right subtree
+        }
+
+        private ref BSTnode FindMin(ref BSTnode node)
+        {
+            if (node.left == null) //reached the leftmost node
+                return ref node;
+            return ref FindMin(ref node.left);
+        }
+
+        private void PrintTree(ref BSTnode node)
+        {
+            if (node != null)
+            {
+                PrintTree(ref node.left);
+                Console.Write(node.data + " ");
+                PrintTree(ref node.right);
+            }
+        }        
+
+        public void PrintTree()
+        {
+            PrintTree(ref root);
+        }
+
+        public void Insert(int input)
+        {
+            Insert(input, ref root);
+        }
+
+        public int Count()
+        {
+            return this.nodes;
+        }
+
+        public int Depth()
+        {
+            return this.depth;
+        }
+
+        public int TheoreticalMinDepth()
+        {
+            return (int)Math.Log(nodes, 2); //Formula for best case binary tree is log base 2 of n in a perfectly balanced tree.
+        }
+
+    }
+}
