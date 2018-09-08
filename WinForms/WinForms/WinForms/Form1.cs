02dda2747c37b7224540324a96c1fda2805fc18c
Isaac <isaac.schultz@wsu.edu>
2018-09-07 19:39:54 -0700

    Visual Studio files added and UI created

---
+++ WinForms/WinForms/WinForms/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            List<int> data = new List<int>();
+            for(int i = 0; i == 10000; i++)
+            {
+                data.Add(rand.Next(20000));
+            }
+
+
+            //Dictionary
+            Dictionary<int,int> dict = new Dictionary<int,int>();
+
+            //O(1) storage complexity
+
+
+            //Sorted O(1) storage and O(n) time complexity
+        }
+    }
+}
