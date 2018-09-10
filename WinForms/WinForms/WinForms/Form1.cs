0adbe18585a9da3996e0009048acfad0e90728fb
Isaac <isaac.schultz@wsu.edu>
2018-09-09 17:46:18 -0700

    Working Sorted storage and Dictionary counting

--- WinForms/WinForms/WinForms/Form1.cs
+++ WinForms/WinForms/WinForms/Form1.cs
@@ -30,26 +30,35 @@ namespace WinForms
 
 
             //Dictionary
-            Dictionary<int, int> dict = new Dictionary<int, int>(); //1    
-            foreach (int item in data)                              //N *
-                if (!dict.ContainsKey(item.GetHashCode()))          //1
-                    dict.Add(item.GetHashCode(), item);             //1
+            Dictionary<int, int> dict = new Dictionary<int, int>();     //1    
+            foreach (int item in data)                                  //N *
+                if (!dict.ContainsKey(item.GetHashCode()))              //1
+                    dict.Add(item.GetHashCode(), item);                 //1
 
-            int dictNumber = dict.Count();                          //1 = O(2 + 2N)
+            int dictNumber = dict.Count();                              //1 = O(2 + 2N)
 
             //O(1) storage complexity
-            int oStorage = 0;                                       //1
-            foreach (int firstItem in data)                         //1
+            int oStorage = 0;  //number of non-dublicate permutations   //1
+            for (int firstItem = 0; firstItem != data.Count(); firstItem++)                             //1
             {
-                foreach (int secondItem in data)                    //1
-                    if (firstItem != secondItem) oStorage++;
+                for (int secondItem = firstItem + 1; secondItem != data.Count(); secondItem++)                         //1
+                    if (data[firstItem] == data[secondItem]) oStorage++;
             }
 
+            //oStorage = oStorage - (data.Count() * data.Count());
+            oStorage /= 2;
 
             //Sorted O(1) storage and O(n) time complexity
 
+            data.Sort();                                                                                            //Ignored
+            int sortedNumber = 0;                                                                                   //1 Storage     1 Time
+            for (int firstItem = 0; firstItem != data.Count(); firstItem++)                                         //1 Storage     N Time
+            {                                                                                                       //                \/
+                while (firstItem != data.Count() - 1 && data[firstItem] == data[firstItem + 1]) firstItem++;        //              Part of N Time
+                sortedNumber++;                                                                                     //              1 Time
+            }
 
-            textBox1.AppendText(dictNumber.ToString() + "\nMemes" + oStorage.ToString());
+            textBox1.AppendText(dictNumber.ToString() + "\t" + oStorage.ToString() + "\t" + sortedNumber.ToString());
         }
     }
 }
