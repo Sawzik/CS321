e1c167d16bd0b21fe4b26d8636d44c8abcc7cc81
Isaac <isaac.schultz@wsu.edu>
2018-09-09 18:41:45 -0700

    All three algorithms working. Needs more comments and correct WinForms implementation

--- WinForms/WinForms/WinForms/Form1.cs
+++ WinForms/WinForms/WinForms/Form1.cs
@@ -26,33 +26,30 @@ namespace WinForms
                 data.Add(rand.Next(20000));
             }
 
-            Console.WriteLine(data.Count);
-
-
             //Dictionary
-            Dictionary<int, int> dict = new Dictionary<int, int>();     //1    
-            foreach (int item in data)                                  //N *
-                if (!dict.ContainsKey(item.GetHashCode()))              //1
-                    dict.Add(item.GetHashCode(), item);                 //1
+            Dictionary<int, int> dict = new Dictionary<int, int>();                                                 //1    
+            foreach (int item in data)                                                                              //N *
+                if (!dict.ContainsKey(item.GetHashCode()))                                                          //  1
+                    dict.Add(item.GetHashCode(), item);                                                             //  1
+            int dictNumber = dict.Count();                                                                          //1 = O(2 + 2N)
 
-            int dictNumber = dict.Count();                              //1 = O(2 + 2N)
 
             //O(1) storage complexity
-            int oStorage = 0;  //number of non-dublicate permutations   //1
-            for (int firstItem = 0; firstItem != data.Count(); firstItem++)                             //1
-            {
-                for (int secondItem = firstItem + 1; secondItem != data.Count(); secondItem++)                         //1
-                    if (data[firstItem] == data[secondItem]) oStorage++;
-            }
+            int oStorage = 0;           //number of dublicates in the list                                          //1
+            for (int firstItem = 0; firstItem < data.Count(); firstItem++)                                          //1                        
+                for (int secondItem = firstItem + 1; secondItem < data.Count(); secondItem++)                       //1                
+                    if (data[firstItem] == data[secondItem]) //If we know there is a duplicate we dont have to check for more
+                    {
+                        oStorage++;
+                        break;
+                    }                        
+            oStorage = data.Count() - oStorage; //subtracts the duplicates from the total number
 
-            //oStorage = oStorage - (data.Count() * data.Count());
-            oStorage /= 2;
 
             //Sorted O(1) storage and O(n) time complexity
-
             data.Sort();                                                                                            //Ignored
             int sortedNumber = 0;                                                                                   //1 Storage     1 Time
-            for (int firstItem = 0; firstItem != data.Count(); firstItem++)                                         //1 Storage     N Time
+            for (int firstItem = 0; firstItem < data.Count(); firstItem++)                                          //1 Storage     N Time
             {                                                                                                       //                \/
                 while (firstItem != data.Count() - 1 && data[firstItem] == data[firstItem + 1]) firstItem++;        //              Part of N Time
                 sortedNumber++;                                                                                     //              1 Time
