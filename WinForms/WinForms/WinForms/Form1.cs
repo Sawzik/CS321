90e332ac86fd3ac4648ac31960c281c0b238874a
Isaac <isaac.schultz@wsu.edu>
2018-09-09 17:05:15 -0700

    created Dictionary counting

--- WinForms/WinForms/WinForms/Form1.cs
+++ WinForms/WinForms/WinForms/Form1.cs
@@ -21,19 +21,35 @@ namespace WinForms
         {
             Random rand = new Random();
             List<int> data = new List<int>();
-            for(int i = 0; i == 10000; i++)
+            for (int i = 0; i != 10000; i++)
             {
                 data.Add(rand.Next(20000));
             }
 
+            Console.WriteLine(data.Count);
+
 
             //Dictionary
-            Dictionary<int,int> dict = new Dictionary<int,int>();
+            Dictionary<int, int> dict = new Dictionary<int, int>(); //1    
+            foreach (int item in data)                              //N *
+                if (!dict.ContainsKey(item.GetHashCode()))          //1
+                    dict.Add(item.GetHashCode(), item);             //1
+
+            int dictNumber = dict.Count();                          //1 = O(2 + 2N)
 
             //O(1) storage complexity
+            int oStorage = 0;                                       //1
+            foreach (int firstItem in data)                         //1
+            {
+                foreach (int secondItem in data)                    //1
+                    if (firstItem != secondItem) oStorage++;
+            }
 
 
             //Sorted O(1) storage and O(n) time complexity
+
+
+            textBox1.AppendText(dictNumber.ToString() + "\nMemes" + oStorage.ToString());
         }
     }
 }
--- WinForms/WinForms/WinForms/Program.cs
+++ WinForms/WinForms/WinForms/Program.cs
@@ -17,6 +17,27 @@ namespace WinForms
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            //Random rand = new Random();
+            //List<int> data = new List<int>();
+            //for (int i = 0; i == 10000; i++)
+            //{
+            //    data.Add(rand.Next(20000));
+            //}
+
+            //Console.WriteLine(data.Count);
+
+
+            ////Dictionary
+            //Dictionary<int, int> dict = new Dictionary<int, int>();
+            //foreach (int item in data)
+            //    dict.Add(item % 13, item);
+            //int dictNumber = dict.Count();
+
+            ////O(1) storage complexity
+
+
+            ////Sorted O(1) storage and O(n) time complexity
         }
     }
 }
