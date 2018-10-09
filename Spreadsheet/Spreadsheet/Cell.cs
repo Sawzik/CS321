7912fe2ca7dc715125e8206389c09ea57d56168c
Isaac <isaac.schultz@wsu.edu>
2018-10-08 17:01:24 -0700

    Cell has some basic propterties

---
+++ Spreadsheet/Spreadsheet/Cell.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Spreadsheet
+{
+    public abstract class Cell : INotifyPropertyChanged
+    {
+        protected int cellColumn;
+        protected int cellRow;
+        protected string cellData;
+        protected string cellParsed;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Cell(int column, int row)
+        {
+            cellColumn = column;
+            cellRow = row;
+        }
+           
+        }
+    }
+}
