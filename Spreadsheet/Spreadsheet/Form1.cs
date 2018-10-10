be677b1c1855d28f171ad2be6057fbd75ef11aed
Isaac <Isaac.Schultz@wsu.edu>
2018-10-10 15:31:27 -0700

    Basic form version of the spreadsheet with no linking to the spreadsheet class

--- Spreadsheet/Spreadsheet/Form1.cs
+++ Spreadsheet/Spreadsheet/Form1.cs
@@ -19,7 +19,38 @@ namespace SpreadsheetForm
         public Form1()
         {
             InitializeComponent();
-
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sheet = new Spreadsheet(26, 50);
+            sheet.CellPropertyChanged += Sheet_CellPropertyChanged; //adds listener to sheets event handler
+            MakeCells();
+            sheet.GetCell(1, 1).Text = "This is a test";
+        }
+
+        private void Sheet_CellPropertyChanged(object sender, EventArgs e)
+        {
+            Cell cell = (Cell)sender; //casts sender as a cell
+            dataGridView1[cell.RowIndex, cell.ColumnIndex].Value = cell.Value; //updates the dataGridView's cells with the cells new value.
+        }
+
+        private void MakeCells()
+        {
+            for (char i = 'A'; i < 'Z' + 1 ; i++) //counts up to Z
+            {
+                DataGridViewTextBoxColumn column = new DataGridViewTextBoxColumn();
+                column.HeaderText = i.ToString();
+                dataGridView1.Columns.Add(column);
+            }
+            for (int i = 0; i < 50; i++) //counts up to Z
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[i].HeaderCell.Value = ((i + 1).ToString());
+            }
+            dataGridView1.RowHeadersWidth = 50; //makes header big enough to see the numbers
+        }
+
+
     }
 }
--- Spreadsheet/Spreadsheet/Properties/Resources.Designer.cs
+++ Spreadsheet/Spreadsheet/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace SpreadsheetForm.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Spreadsheet.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SpreadsheetForm.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
