8b24f89137ecd6f03db65d6139007e0dbe0a0632
Isaac <Isaac.Schultz@wsu.edu>
2018-10-21 16:37:40 -0700

    Function protoypes and class files created

---
+++ Spreadsheet/SpreadsheetEngine/ExpTree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CptS321
+{
+    class ExpTree
+    {
+        ExpTree(string expression)
+        {
+
+        }
+
+        void SetVar(string varName, double varValue)
+        {
+
+        }
+
+        public double Eval()
+        {
+            return 0; //placeholder
+        }
+    }
+}
