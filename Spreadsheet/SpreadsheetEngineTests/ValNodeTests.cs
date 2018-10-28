9543069fb039850d86e5f490a9abea2df32a8e7f
Isaac <Isaac.Schultz@wsu.edu>
2018-10-27 18:58:04 -0700

    Added tests for ValNode

---
+++ Spreadsheet/SpreadsheetEngineTests/ValNodeTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CptS321;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CptS321.Tests
+{
+    [TestClass()]
+    public class ValNodeTests
+    {
+        [TestMethod()]
+        public void ValNodeTest()
+        {
+            // Arrange
+            double testValue = 1587.34;
+
+            // Act
+            ValNode testVarNode = new ValNode(testValue);
+
+            // Assert
+            Assert.AreEqual(testVarNode.Value, testValue);
+        }
+
+        [TestMethod()]
+        public void EvalTest()
+        {
+            // Arrange
+            double testValue = 1587.34;
+
+            // Act
+            ValNode testVarNode = new ValNode(testValue);
+
+            // Assert
+            Assert.AreEqual(testVarNode.Eval(), testValue);
+        }
+    }
+}
