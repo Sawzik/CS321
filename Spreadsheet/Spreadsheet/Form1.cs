e26894c12bf8d5d898575ca2f47a420075c3e98a
Isaac <isaac.schultz@wsu.edu>
2018-11-04 20:38:59 -0800

    Added a textbox to the form

--- Spreadsheet/Spreadsheet/Form1.cs
+++ Spreadsheet/Spreadsheet/Form1.cs
@@ -71,5 +71,10 @@ namespace SpreadsheetForm
         {
             DemoCells();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
