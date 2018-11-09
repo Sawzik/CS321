54d0f4a42a3eb88deb1ca65919be0d75bce21c01
Isaac <isaac.schultz@wsu.edu>
2018-11-09 11:28:30 -0800

    I dont know what changed

--- Spreadsheet/SpreadsheetEngine/Cell.cs
+++ Spreadsheet/SpreadsheetEngine/Cell.cs
@@ -45,14 +45,15 @@ namespace CptS321
 
         protected void OnPropertyChanged(string name)
         {
-
+            if (name == "Text")
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name)); //fancy way of only using PropertyChanged if it isn't null
-           
+            if (name == "Value")
+                ValueChanged?.Invoke(this, new PropertyChangedEventArgs(name)); //fancy way of only using PropertyChanged if it isn't null
         }
 
         public void OnValueChanged(object sender, PropertyChangedEventArgs e)
         {
-            OnPropertyChanged("Value");
+            OnPropertyChanged("Text");
         }
 
     }
